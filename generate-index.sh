c2d48fd6b93c5b2486f8597e912ebc13c78e2fab
Jérémie Huchet <jeremiehuchet@users.noreply.github.com>
2016-03-02 21:52:20 +0100

    update generate-index script

--- generate-index.sh
+++ generate-index.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-for i in $(cd _ansible ; git tag -l | grep ^v | tac) ; do
+function startswith() {
+  if [ $(echo $1 | grep -c "^$2") -eq 0 ] ; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+for i in $(cd _docs ; ls -1d v* | tac) ; do
+   color="default"
+   startswith $i '2.0' && color="success"
    echo "        <div class=\"col-lg-4\">
-          <a href=\"$i\" class=\"btn btn-default\" title=\"Ansible $i documentation\">$i</a>
+          <a href=\"$i\" class=\"btn btn-$color\" title=\"Ansible $i documentation\">$i</a>
         </div>"
 done
 
