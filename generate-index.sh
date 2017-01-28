0fa9b98f827c6a85931f32ea2f68438e6f8b3295
Jérémie Huchet <jeremiehuchet@users.noreply.github.com>
2017-01-28 15:15:23 +0100

    update sitemap and index generators

--- generate-index.sh
+++ generate-index.sh
@@ -1,18 +1,88 @@
 #!/bin/bash
 
-function startswith() {
-  if [ $(echo $1 | grep -c "^$2") -eq 0 ] ; then
-    return 1
-  else
-    return 0
-  fi
-}
+cat - <<EOF
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Documentation for almost all Ansible versions">
+    <meta name="author" content="">
 
-for i in $(cd _docs ; ls -1d v* | tac) ; do
-   color="default"
-   startswith $i '2.0' && color="success"
-   echo "        <div class=\"col-lg-4\">
-          <a href=\"$i\" class=\"btn btn-$color\" title=\"Ansible $i documentation\">$i</a>
-        </div>"
+    <title>Documentation for almost all Ansible versions</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <link href="style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="https://www.ansible.com/">Ansible</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Ansible documentation</h3>
+      </div>
+
+      <div class="jumbotron">
+        <h1>Ansible documentation</h1>
+        <p class="lead">You're stuck with an older Ansible version? Here is the documentation you need.</p>
+      </div>
+
+EOF
+
+for majorMinor in $(find _docs/ -maxdepth 1 -name 'v*' -printf "%f\n" | cut -d '/' -f 2 | sed 's/^v\([0-9]\+\)\.\([0-9]\+\).*$/\1.\2/g' | sort --reverse | uniq) ; do
+
+cat - <<EOF
+      <div class="row marketing">
+        <div class="col-lg-12">
+          <h2>Ansible $majorMinor</h2>
+        </div>
+EOF
+
+  # mark latest version
+  color="success"
+
+  for version in $(find _docs/ -maxdepth 1 -name "v$majorMinor*" -printf "%f\n" | sort --reverse) ; do
+
+cat - <<EOF
+
+        <div class="col-lg-4">
+          <a href="$version" class="btn btn-$color" title="Ansible $version documentation">$version</a>
+        </div>
+EOF
+
+    color="default"
+
+  done
+
+cat - <<EOF
+
+      </div>
+
+EOF
+
 done
 
+cat - <<EOF
+
+      <footer class="footer">
+        <p>Hosted on Github Pages</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
+EOF
--- generate-sitemap.sh
+++ generate-sitemap.sh
@@ -3,16 +3,27 @@
 date=$1
 baseurl=$2
 
-echo '<?xml version="1.0" encoding="UTF-8"?>'
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+cat - <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>$baseurl</loc>
+    <lastmod>$date</lastmod>
+    <priority>0.1</priority>
+  </url>
+EOF
 
 for i in $(cd _docs ; ls -1d v* | tac) ; do
-  echo '  <url>'
-  echo "    <loc>$baseurl/$i</loc>"
-  echo "    <lastmod>$date</lastmod>"
-  echo '    <changefreq>never</changefreq>'
-  echo '    <priority>0.1</priority>'
-  echo '  </url>'
+
+cat - <<EOF
+  <url>
+    <loc>$baseurl/$i</loc>
+    <lastmod>$date</lastmod>
+    <changefreq>never</changefreq>
+    <priority>0.1</priority>
+  </url>
+EOF
+
 done
 
 echo '</urlset>'
